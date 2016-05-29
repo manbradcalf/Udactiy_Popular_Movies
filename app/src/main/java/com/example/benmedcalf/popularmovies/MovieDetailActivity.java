fb61a2318cb16b79795ff3af5f0d55d114fb990b
Ben Medcalf <ben.medcalf@snagajob.com>
2016-05-29 18:27:55 -0400

    First commit, cant figure out how to get the image in the CollapsingToolbar

--- app/src/main/java/com/example/benmedcalf/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/com/example/benmedcalf/popularmovies/MovieDetailActivity.java
@@ -3,6 +3,7 @@ package com.example.benmedcalf.popularmovies;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
@@ -20,6 +21,7 @@ public class MovieDetailActivity extends AppCompatActivity {
     public static final String BASE_URL_FOR_IMAGES = "http://image.tmdb.org/t/p/w185/";
     public TextView mDescription;
     public ImageView mPoster;
+    public CollapsingToolbarLayout mCollapsingToolbarLayout;
 
     public static Intent newIntent(Context packageContext, Movie movie) {
         Intent intent = new Intent(packageContext, MovieDetailActivity.class);
@@ -32,17 +34,21 @@ public class MovieDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.movie_detail_activity);
-        mDescription = (TextView) findViewById(R.id.detail_text_view);
-        mPoster = (ImageView) findViewById(R.id.detail_poster_image_view);
         Intent intent = getIntent();
         Movie movie = intent.getParcelableExtra(EXTRA_MOVIE_ID);
         String description = movie.getOverview();
 
-        mDescription.setText(description);
+        mDescription = (TextView) findViewById(R.id.description);
+        mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        mCollapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
 
+        mPoster = (ImageView) findViewById(R.id.movie_poster_detail);
         Picasso.with(this)
                 .load(BASE_URL_FOR_IMAGES + movie.getPosterPath())
-                .placeholder(R.color.colorAccent).into(mPoster);
+                .placeholder(R.color.colorAccent)
+                .into(mPoster);
+
+        mDescription.setText(description);
 
         Log.d(MovieDetailActivity.class.getSimpleName(), "Launched Movie Detail Activity");
     }
