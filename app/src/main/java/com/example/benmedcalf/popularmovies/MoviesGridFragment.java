af33bab47253776edfe5ba7bfff19ec3b76159ef
manbradcalf <ben.medcalf@gmail.com>
2016-07-21 16:43:40 -0400

    Added eventbus methods to GridFragment. Not crashing on launch. Need to set up onEvent and MainActivity to use movieDetailFragment instead of the moviedetailactivity

--- app/src/main/java/com/example/benmedcalf/popularmovies/MoviesGridFragment.java
+++ app/src/main/java/com/example/benmedcalf/popularmovies/MoviesGridFragment.java
@@ -17,16 +17,16 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.benmedcalf.popularmovies.Adapter.MoviesAdapter;
+import com.example.benmedcalf.popularmovies.Event.MovieSelectedEvent;
 import com.example.benmedcalf.popularmovies.Model.Example;
 import com.example.benmedcalf.popularmovies.Model.Movie;
-import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import de.greenrobot.event.EventBus;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -93,6 +93,26 @@ public class MoviesGridFragment extends android.support.v4.app.Fragment {
     }
 
     @Override
+    public void onResume()
+    {
+        super.onResume();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        EventBus.getDefault().unregister(this);
+    }
+
+    public void onEvent(MovieSelectedEvent event) {
+        Intent intent = MovieDetailActivity.newIntent(getActivity(), event.getMovie());
+        startActivity(intent);
+    }
+
+
+
+        @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
         menuInflater.inflate(R.menu.menu_movies_grid_fragment, menu);
     }
@@ -189,82 +209,5 @@ public class MoviesGridFragment extends android.support.v4.app.Fragment {
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt(SORT_TYPE, mSortPreference);
-    }
-
-    /** View Holder Class **/
-
-    public class MovieViewHolder extends RecyclerView.ViewHolder
-            implements View.OnClickListener {
-
-        public ImageView mImageView;
-        public Movie mMovie;
-
-        public void setMovie(Movie movie) {
-            mMovie = movie;
-        }
-
-        public MovieViewHolder(View itemView) {
-            super(itemView);
-            mImageView = (ImageView) itemView.findViewById(R.id.poster_image_view);
-            itemView.setOnClickListener(this);
-        }
-
-        @Override
-        public void onClick(View v) {
-                Bundle bundle = new Bundle();
-                bundle.putParcelable(EXTRA_MOVIE_ID, mMovie);
-                MovieDetailFragment movieDetailFragment = new MovieDetailFragment();
-
-                getFragmentManager().beginTransaction()
-                        .replace(R.id.movie_detail_fragment_frame, movieDetailFragment,
-                               DETAILFRAGMENT_TAG).commit();
-            }
-    }
-
-    /** Movies Adapter Class **/
-
-    public class MoviesAdapter
-            extends RecyclerView.Adapter<MoviesGridFragment.MovieViewHolder> {
-
-        public List<Movie> mMovieList;
-        private LayoutInflater mInflater;
-        private Context mContext;
-        public static final String BASE_URL_FOR_IMAGES = "http://image.tmdb.org/t/p/w185/";
-
-
-        public MoviesAdapter(Context context) {
-            this.mContext = context;
-            this.mInflater = LayoutInflater.from(context);
-            this.mMovieList = new ArrayList<>();
-        }
-
-        @Override
-        public MoviesGridFragment.MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = mInflater.inflate(R.layout.movie_card, parent, false);
-            return new MoviesGridFragment.MovieViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(MoviesGridFragment.MovieViewHolder holder, int position) {
-
-            Movie movie = mMovieList.get(position);
-            holder.setMovie(movie);
-
-            //Load image
-            Picasso.with(mContext)
-                    .load(BASE_URL_FOR_IMAGES + movie.getPosterPath())
-                    .placeholder(R.color.colorAccent).into(holder.mImageView);
-        }
-
-        @Override
-        public int getItemCount() {
-            return (mMovieList == null) ? 0 : mMovieList.size();
-        }
-
-        public void setMovieList(List<Movie> movieList) {
-            this.mMovieList.clear();
-            this.mMovieList.addAll(movieList);
-            notifyDataSetChanged();
-        }
     }
 }
